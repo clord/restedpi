91e7b11bd35ca0fb3a524429081f9625b13809d2
Christopher Lord <christopher@pliosoft.com>
2019-08-25 13:51:16 -0600

    adding more warp stuff

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use std::error::Error;
 //use rppal::i2c::I2c;
 use rppal::system::DeviceInfo;
+use warp::{self, path, Filter};
 
 fn main() -> Result<(), Box<dyn Error>> {
     //let i2c = I2c::new()?;
+    let serverName = DeviceInfo::new()?.model()
 
-    println!("Going to talk on I2C of {}", DeviceInfo::new()?.model());
+    println!("    Running on {}", serverName);
+
+    let restedpiserver = path!("bmp085" / String).map(|index| serverName)
 
     Ok(())
 }
