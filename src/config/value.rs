4b20e934089e8212877860034595b8238c947143
Christopher Lord <christopher@pliosoft.com>
2020-11-09 08:35:27 -0700

    solar offset for sunset

--- src/config/value.rs
+++ src/config/value.rs
@@ -159,7 +159,7 @@ pub fn evaluate(app: &State, expr: &Value) -> Result<f64> {
             .to_degrees()
                 / 15.0;
             let exact_offset = sched::exact_offset_hrs(evaluate(app, long)?);
-            let solar_offset = (12.0 + 2.0 * (12.0 - h)) * 3600.0;
+            let solar_offset = (24.0 - h) * 3600.0;
             let solar_dt = FixedOffset::east((exact_offset * 3600.0) as i32)
                 .yo(dt.year(), doy_ev as u32)
                 .and_hms(0, 0, 0)
