3bcd65676c32e373c30f641d36de4613dda1c4b0
Christopher Lord <christopher@pliosoft.com>
2024-01-12 23:49:27 -0700

    switch to using flake-parts

--- flake.nix
+++ flake.nix
@@ -4,11 +4,13 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    sops-nix.url = "github:Mic92/sops-nix";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { self, nixpkgs, sops-nix, flake-utils, rust-overlay, ... }:
-    flake-utils.lib.eachSystem [ "aarch64-linux" "aarch64-darwin" ] (system:
+  outputs = inputs@{ self, nixpkgs, flake-parts, flake-utils, rust-overlay, ... }: 
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      perSystem = { self', pkgsm, lib, system, ... }:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
@@ -28,120 +30,17 @@
           buildFeatures = [ "raspberrypi" ];
         };
 
-        rpi4System = nixpkgs.lib.nixosSystem {
-          system = "aarch64-linux";
-          modules = [
-            sops-nix.nixosModules.sops
-            {
-              sops.defaultSopsFile = ./secrets/rpi.yaml;
-              sops.age.keyFile = "/var/lib/sops/age/keys.txt";
-              sops.age.generateKey = true;
-
-              # Expose as /run/secrets/application_secret
-              sops.secrets.application_secret = { };
-              sops.secrets.configuration = { };
-              # Expose as /run/secrets/rpi_cert
-              sops.secrets.rip_cert = { };
-              # Expose as /run/secrets/rpi_key
-              sops.secrets.rip_key = { };
-
-              # mkpasswd -m sha-512
-              sops.secrets.clord_password = { neededForUsers = true; };
-            }
-            "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
-            ({ config, ... }: {
-              config = {
-
-                time.timeZone = "America/Edmonton";
-                services.timesyncd.enable = true;
-                i18n.defaultLocale = "en_CA.UTF-8";
-                sdImage.compressImage = false;
-                system = { stateVersion = "23.11"; };
-
-                networking = {
-                  hostName = "chickenpi";
-                  wireless.enable = false;
-                  useDHCP = true;
-                };
-
-                hardware.bluetooth.powerOnBoot = false;
-
-                users = {
-                  mutableUsers = false;
-                  users.clord = {
-                    isNormalUser = true;
-                    description = "Christopher Lord";
-                    hashedPasswordFile =
-                      config.sops.secrets.clord_password.path;
-                    extraGroups = [ "wheel" ];
-                    openssh.authorizedKeys.keys = [
-                      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP3DeyWHOIc+SdTqNP9iFD4jpf0fg1FVTsaWn2qcKDTa clord@edmon"
-                      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINXLYw43gNlnfEoHpmK/UWae4DcQyLBQTGQH9ZYlRG5q clord@wildwood"
-                      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINLtiIXQ0r+l0gtnjCj1hT5Z1YzRqgJ/g66pP/eEuXM3 clord@ipad"
-                      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHTOl4xwPOT82EmW5bEBpWyi5Iy9ZEYWPToJEQjIagyO clord@1p"
-                    ];
-                  };
-                };
-                environment.systemPackages =
-                  [ pkgs.git pkgs.htop pkgs.vim rustBuild ];
-                nix = {
-                  package = pkgs.nix;
-                  settings = {
-                    auto-optimise-store = true;
-                    max-jobs = 4;
-                    cores = 4;
-                    trusted-users = [ "root" "clord" "@wheel" ];
-                    experimental-features = [ "nix-command" "flakes" ];
-                  };
-                  gc = {
-                    automatic = true;
-                    dates = "weekly";
-                    options = "--delete-older-than 60d";
-                  };
-                  # Free up to 1GiB whenever there is less than 100MiB left.
-                  extraOptions = "  min-free = ${
-                        toString (100 * 1024 * 1024)
-                      }\n  max-free = ${
-                        toString (1024 * 1024 * 1024)
-                      }\n  keep-outputs = true\n  keep-derivations = true\n";
-                };
-                services.openssh = {
-                  enable = true;
-                  settings = {
-                    PermitRootLogin = "yes";
-                    PasswordAuthentication = false;
-                    KbdInteractiveAuthentication = false;
-                  };
-                };
-                services.prometheus = {
-                  exporters = {
-                    node = {
-                      enable = true;
-                      enabledCollectors = [ "systemd" ];
-                      port = 9002;
-                    };
-                  };
-                };
-                systemd.services.restedpi = {
-                  enable = true;
-                  environment = { RUST_BACKTRACE = "1"; };
-                  description = "Runs the restedpi system";
-                  unitConfig = {
-
-                  };
-                  serviceConfig = {
-                    ExecStart =
-                      "${rustBuild}/bin/restedpi --config-file /run/secrets/configuration --log-level warn server";
-                  };
-                  wantedBy = [ "multi-user.target" ];
-                };
-              };
-            })
-          ];
-        };
       in {
-        defaultPackage.aarch64_linux = rustBuild;
-        devShell = pkgs.mkShell { buildInputs = with pkgs; [ nixfmt sqlite ]; };
+        packages = rec {
+          restedpi = rustBuild;
+          default = restedpi;
+        };
+
+        devShells.default = pkgs.mkShell { buildInputs = with pkgs; [ 
+          nixfmt 
+          sqlite 
+          rustVersion 
+        ]; };
 
         apps = {
           info = flake-utils.lib.mkApp {
@@ -150,6 +49,6 @@
             '';
           };
         };
-        rpi4 = rpi4System.config.system.build.sdImage;
-      });
+      };
+  };
 }
